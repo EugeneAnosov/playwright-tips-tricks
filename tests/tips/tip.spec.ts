e215c9796178d88f442b2c03d65ac1c0d82c55cd
yevhen.anosov <yevhen.anosov@similarweb.com>
2022-03-30 16:56:34 +0300

    Added a couple of tests

---
+++ tests/tips/tip.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+test.describe.only('Tips & Tricks Section', () => {
+    test('TestInfo Object', async ({ page }, testInfo) => {
+        await page.goto('https://example.com')
+        //console.log(testInfo)
+    })
+
+    test.skip('Test Skip Browser', async ({ page, browserName }) => {
+        test.skip(browserName === 'chromium', 'Feature not ready in Chrome browser')
+        await page.goto('https://example.com')
+    })
+
+    test('Test Fixme Annotation', async ({ page, browserName }) => {
+        test.fixme(browserName === 'chromium', 'Test is not stable, needs revision')
+        await page.goto('https://example.com')
+    })
+
+    const people = ['Mike', 'Judy', 'Peter', 'Elon', 'Alice']
+    for (const name of people) {
+        test(`Running test fo ${name}`, async ({ page }) => {
+            await page.goto('http://zero.webappsecurity.com/index.html')
+            await page.type('#searchTerm', `${name}`)
+            await page.waitForTimeout(1000)
+        })
+    }
+})
