305b6924ad8f8c2fb8f8394ca1f570d96bcfc79d
yevhen.anosov <yevhen.anosov@similarweb.com>
2022-03-31 21:33:47 +0300

    Data helpers added

--- tests/tips/tip.spec.ts
+++ tests/tips/tip.spec.ts
@@ -1,35 +1,72 @@
-import { test, expect } from "@playwright/test";
+import {test, expect} from "@playwright/test";
+import {getRandomEmail, getRandomNumber, getRandomString} from "../../utils/data-helpers";
 
-test.describe.only('Tips & Tricks Section', () => {
-    test('TestInfo Object', async ({ page }, testInfo) => {
-        await page.goto('https://example.com')
-        //console.log(testInfo)
-    })
+test.describe.only('Tips & Tricks Section',
+    () => {
+        // Device Simulation
+        // npx playwright open --device="iPhone 11" wikipedia.org
 
-    test.skip('Test Skip Browser', async ({ page, browserName }) => {
-        test.skip(browserName === 'chromium', 'Feature not ready in Chrome browser')
-        await page.goto('https://example.com')
-    })
+        // PDF Saving
+        // npx playwright pdf https://example.com my-file.pdf
 
-    test('Test Fixme Annotation', async ({ page, browserName }) => {
-        test.fixme(browserName === 'chromium', 'Test is not stable, needs revision')
-        await page.goto('https://example.com')
-    })
+        // Generate Customize Screenshot
+        // npx playwright screenshot --device='iPhone 12' --color-scheme=dark --wait-for-timeout=3000 twitter.com twitter-iphone-image.png
 
-    const people = ['Mike', 'Judy', 'Peter', 'Elon', 'Alice']
-    for (const name of people) {
-        test(`Running test fo ${name}`, async ({ page }) => {
-            await page.goto('http://zero.webappsecurity.com/index.html')
-            await page.type('#searchTerm', `${name}`)
-            await page.waitForTimeout(1000)
+        // Change Timezone and Localization
+        // npx playwright open --timezone "Europe/Rome" --lang="it-IT" google.com //--geolocation="40.121, 10.123"
+
+        test('TestInfo Object', async ({page}, testInfo) => {
+            await page.goto('https://example.com')
+            //console.log(testInfo)
         })
-    }
 
-    test('Mouse Movement Simulation', async ({ page }) => {
-        await page.goto('https://example.com')
-        await page.mouse.move(0, 0)
-        await page.mouse.down()
-        await page.mouse.move(0, 100)
-        await page.mouse.up()
+        test.skip('Test Skip Browser', async ({page, browserName}) => {
+            test.skip(browserName === 'chromium', 'Feature not ready in Chrome browser')
+            await page.goto('https://example.com')
+        })
+
+        test('Test Fixme Annotation', async ({page, browserName}) => {
+            test.fixme(browserName === 'chromium', 'Test is not stable, needs revision')
+            await page.goto('https://example.com')
+        })
+
+        const people = ['Mike', 'Judy', 'Peter', 'Elon', 'Alice']
+        for (const name of people) {
+            test(`Running test fo ${name}`, async ({page}) => {
+                await page.goto('http://zero.webappsecurity.com/index.html')
+                await page.type('#searchTerm', `${name}`)
+                await page.waitForTimeout(1000)
+            })
+        }
+
+        test('Mouse Movement Simulation', async ({page}) => {
+            await page.goto('https://example.com')
+            await page.mouse.move(0, 0)
+            await page.mouse.down()
+            await page.mouse.move(0, 100)
+            await page.mouse.up()
+        })
+
+        test('Multiple Browser Tabs inside one browser', async ({browser}) => {
+            const context = await browser.newContext()
+            const page1 = await context.newPage()
+            const page2 = await context.newPage()
+            const page3 = await context.newPage()
+
+            await page1.goto('https://example.com')
+            await page2.goto('https://example.com')
+            await page3.goto('https://example.com')
+
+            await page1.waitForTimeout(5000)
+        })
+
+        test.only('Random Number', async ({ page}) => {
+            await page.goto('https://example.com')
+            let newNumber = await getRandomNumber()
+            let newString = await getRandomString()
+            let newEmail = await getRandomEmail()
+            console.log(newNumber)
+            console.log(newString)
+            console.log(newEmail + '_automation' + '@' + 'gmail.com')
+        })
     })
-})
---
+++ utils/data-helpers.ts
@@ -0,0 +1,13 @@
+const crypto = require("crypto")
+
+export async function getRandomNumber() {
+    return Math.floor(Math.random() * 10000 + 1)
+}
+
+export async function getRandomString() {
+    return crypto.randomBytes(20).toString('hex')
+}
+
+export async function getRandomEmail() {
+    return crypto.randomBytes(5).toString('hex')
+}
